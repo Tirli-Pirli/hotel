6115d8160d89aa4053dc25586a9004bb5a19c91a
Jurgis <audickasj@gmail.com>
2020-02-03 21:17:53 +0200

    update03

--- src/index.js
+++ src/index.js
@@ -26,12 +26,5 @@ import './assets/images/palms.jpg';
 import './assets/images/newmnu.svg';
 import './assets/images/bg-66.jpg'
 
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
 
 
